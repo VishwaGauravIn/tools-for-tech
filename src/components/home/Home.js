cec3e31108d41170ca7965781a84df283afd9055
VishwaGauravIn <vishwagauravin@gmail.com>
2021-10-15 21:29:25 +0530

    +responsiveness

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,7 +9,7 @@ export default function Home(params) {
       Tools for Tech
     </div>
     <div className="home-desc">
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br/> Aliquam delectus quas vitae ut aperiam eum saepe, inventore <br/> modi, laboriosam dolor minus maxime rem doloremque <br/> earum repellendus error perferendis unde vero illum! <br/> Est fugiat molestiae ipsam amet?
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam delectus quas vitae ut aperiam eum saepe, inventore modi, laboriosam dolor minus maxime rem doloremque earum repellendus error perferendis unde vero illum! Est fugiat molestiae ipsam amet?
     </div>
     </div>
     <List/>
