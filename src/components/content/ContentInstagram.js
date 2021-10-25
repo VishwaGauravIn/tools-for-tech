4d55fe5841026e8b0f752e30715b58f7e880c7fc
VishwaGauravIn <vishwagauravin@gmail.com>
2021-10-25 22:35:16 +0530

    minor home rework

--- src/components/content/ContentInstagram.js
+++ src/components/content/ContentInstagram.js
@@ -49,8 +49,8 @@ function ContentInstagram(){
             color="bg-gradient-7"
             title="Unfollow"
             description="Instagram Account Management Tool to help you unfollow users who do not follow you and follow back users who follow you. Here's an amazing, easy and free app to help you with various features such as: Track your non-followers on Instagram, Just Unfollow users who do not follow you back, Mass Unfollow users, Know your fans - users who follow you."
-            urlLink="https://play.google.com/store/apps/details?id=easy.follow.unfollow"
-            logo="https://play-lh.googleusercontent.com/_-JW8lMHX3OoMxu0uaEkcJ6YO3KjIIrOQrLnZSvKy501c6dLlqADeATJpyjbvz8RSw-l=s180-rw" 
+            urlLink="https://play.google.com/store/apps/details?id=com.follow.unfollow"
+            logo="https://play-lh.googleusercontent.com/5JUAFgq-W1-9YgOi_SL3jBES4qaxXrBCJNPNJH4PDferj4iFASaUbU0PE83YaolwZDeu=s180-rw" 
         /> 
         </div>
         </div>              
